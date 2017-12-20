2e74dd0cc8eddbd3b745c98dc529fb529dd328ef
TiesP <pavel.o.71@gmail.com>
2017-12-20 14:13:00 +0300

    details

--- hw.py
+++ hw.py
@@ -4,14 +4,48 @@ class Animal:
     age = 0
     amount_feed_per_day = 0
 
+    def __init__(self, amount_feed_per_day):
+        self.amount_feed_per_day = amount_feed_per_day
+
     def count_feed_for_period(self, period):
         print("Count the feed for the period:", period,
               self.amount_feed_per_day * period)
 
 
 class Mammal(Animal):
-    pass
+    gives_milk = False
 
 
 class Bird(Animal):
+    gives_eggs = False
+
+
+class Cow(Mammal):
+    gives_milk = True
+
+
+class Goat(Mammal):
+    gives_milk = True
+
+
+class Sheep(Mammal):
     pass
+
+
+class Pig(Mammal):
+    pass
+
+
+class Duck(Bird):
+    gives_eggs = True
+
+
+class Chicken(Bird):
+    gives_eggs = True
+
+
+class Goose(Bird):
+    gives_eggs = True
+
+d = Duck(10)
+d.count_feed_for_period(5)
