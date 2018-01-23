8ccb2dd21ff0c3f41f11b271577e2a985cdb6fd6
TiesP <pavel.o.71@gmail.com>
2018-01-23 13:41:54 +0300

    list из кубов всех чисел!

--- hw14.py
+++ hw14.py
@@ -1,6 +1,6 @@
 # необходимо получить list из кубов всех чисел, делящихся и на 3,
 # и на 4 без остатка, взятых из массива чисел mass.
 # Использовать List Comprehensions.
-mass = [1, 3, 5, 12, 13, 24]
-result = [n for n in mass if not n % 12]
+mass = [1, 2, 3, 4, 5, 12, 13, 18, 20, 24]
+result = [n**3 for n in mass if not n % 12]
 print(result)
